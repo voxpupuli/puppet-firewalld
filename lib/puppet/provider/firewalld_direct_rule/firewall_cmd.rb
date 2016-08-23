bbd9bfe57cda4c0e8e449e88091da7848bdfd951
Craig Dunn <craig@craigdunn.org>
2016-08-23 08:19:40 +0200

    tidied up spacing and added comments

--- lib/puppet/provider/firewalld_direct_rule/firewall_cmd.rb
+++ lib/puppet/provider/firewalld_direct_rule/firewall_cmd.rb
@@ -23,18 +23,23 @@ Puppet::Type.type(:firewalld_direct_rule).provide(
     execute_firewall_cmd(['--direct', '--remove-rule', @rule_args], nil)
   end
 
+  # Arguments should be parsed as separate array entities, but quoted arg
+  # eg --log-prefix 'IPTABLES DROPPED' should include the whole quoted part
+  # in one element without the quotes.
+  #
   def parse_args(args)
     args_array = args.split(/(\'[^\']*\'| )/).reject { |r| [ "", " "].include?(r) }
     args_array.map { |a| a.delete("'") }
   end
 
+
   def generate_raw
     rule = []
     rule << [
-	    @resource[:inet_protocol],
-	    @resource[:table],
-	    @resource[:chain],
-	    @resource[:priority].to_s,
+      @resource[:inet_protocol],
+      @resource[:table],
+      @resource[:chain],
+      @resource[:priority].to_s,
       parse_args(@resource[:args])
     ]
     rule.flatten

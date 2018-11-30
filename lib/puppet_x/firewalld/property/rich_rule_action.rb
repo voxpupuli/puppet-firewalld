839abb5c7089bb6ac0ae12d5cba738697b33e0ad
bmagistro <koncept1@gmail.com>
2018-11-30 18:42:52 -0500

    #193 changes to get rich rule working in 5.5, not expecting this to be merged

--- lib/puppet_x/firewalld/property/rich_rule_action.rb
+++ lib/puppet_x/firewalld/property/rich_rule_action.rb
@@ -7,10 +7,10 @@ module PuppetX
         end
         validate do |value|
           if value.is_a?(Hash)
-            if value.keys.sort != %i[action type]
+            if value.keys.sort != ["action", "type"]
               raise Puppet::Error, "Rule action hash should contain `action` and `type` keys. Use a string if you only want to declare the action to be `accept` or `reject`. Got #{value}"
             end
-            _validate_action(value[:action])
+            _validate_action(value["action"])
           elsif value.is_a?(String)
             _validate_action(value)
           end

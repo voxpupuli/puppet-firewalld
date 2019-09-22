de9bd732cdccf47dc8da773b3a9e9e01e023ec1a
Alexander Fisher <alex@linfratech.co.uk>
2019-09-22 12:27:20 +0100

    Rubocop Autofix Layout/LeadingCommentSpace

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -4,9 +4,9 @@ describe 'firewalld', unless: UNSUPPORTED_PLATFORMS.include?(fact('osfamily')) d
   context 'running with defaults' do
     it 'runs successfully' do
       pp = 'include firewalld'
-      #Apply...
+      # Apply...
       apply_manifest(pp, catch_failures: true)
-      #...twice and see if this is idempotent
+      # ...twice and see if this is idempotent
       expect(apply_manifest(pp, catch_failures: true).exit_code).to be_zero
     end
 

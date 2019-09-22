ec7a2c7f78d2aa876c085f3d236e81411c5e6dbd
Alexander Fisher <alex@linfratech.co.uk>
2019-09-22 12:47:10 +0100

    Rubocop Autofix Lint/UnusedMethodArgument

--- lib/puppet/provider/firewalld_zone/firewall_cmd.rb
+++ lib/puppet/provider/firewalld_zone/firewall_cmd.rb
@@ -39,7 +39,7 @@ Puppet::Type.type(:firewalld_zone).provide(
     zone_target
   end
 
-  def target=(t)
+  def target=(_t)
     debug("Setting target for zone #{@resource[:name]} to #{@resource[:target]}")
     execute_firewall_cmd(['--set-target', @resource[:target]])
   end

2c69c742a147a7b13d11b990e5971f3538f06160
Alexander Fisher <alex@linfratech.co.uk>
2019-09-22 12:45:03 +0100

    Rubocop Autofix Lint/StringConversionInInterpolation

--- lib/puppet/provider/firewalld_direct_purge/firewall_cmd.rb
+++ lib/puppet/provider/firewalld_direct_purge/firewall_cmd.rb
@@ -9,13 +9,13 @@ Puppet::Type.type(:firewalld_direct_purge).provide(
 
   def get_instances_of(restype)
     raise Puppet::Error, "Unknown type #{restype}" unless [:chain, :passthrough, :rule].include?(restype)
-    perm = execute_firewall_cmd(['--direct', "--get-all-#{restype.to_s}s"], nil).split(%r{\n})
-    curr = execute_firewall_cmd(['--direct', "--get-all-#{restype.to_s}s"], nil, false).split(%r{\n})
+    perm = execute_firewall_cmd(['--direct', "--get-all-#{restype}s"], nil).split(%r{\n})
+    curr = execute_firewall_cmd(['--direct', "--get-all-#{restype}s"], nil, false).split(%r{\n})
     [perm, curr].flatten.uniq
   end
 
   def purge_resources(restype, args)
     raise Puppet::Error, "Unknown type #{restype}" unless [:chain, :passthrough, :rule].include?(restype)
-    execute_firewall_cmd(['--direct', "--remove-#{restype.to_s}", parse_args(args)], nil)
+    execute_firewall_cmd(['--direct', "--remove-#{restype}", parse_args(args)], nil)
   end
 end

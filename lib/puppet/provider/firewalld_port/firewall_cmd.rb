6e471be8c703caf43d4520913a54066bcc13af98
isqo <qouiqaismail@gmail.com>
2020-04-03 23:04:50 +0200

    FIX #276 for firewalld_port - Check port's existence in both runtime and permanent

--- lib/puppet/provider/firewalld_port/firewall_cmd.rb
+++ lib/puppet/provider/firewalld_port/firewall_cmd.rb
@@ -7,12 +7,25 @@ Puppet::Type.type(:firewalld_port).provide(
 ) do
   desc 'Interact with firewall-cmd'
 
+  attr_accessor :in_perm, :in_run
+
   mk_resource_methods
+
+  def initialize(value = {})
+    super(value)
+    @in_perm = false
+    @in_run = false
+  end
 
   def exists?
     @rule_args ||= build_port_rule
-    output = execute_firewall_cmd(['--query-port', @rule_args], @resource[:zone], true, false)
-    output.exitstatus.zero?
+    @in_perm = execute_firewall_cmd(['--query-port', @rule_args], @resource[:zone], true, false).exitstatus.zero?
+    @in_run = execute_firewall_cmd(['--query-port', @rule_args], @resource[:zone], false, false).exitstatus.zero?
+    if @resource[:ensure] == :present
+      @in_perm && @in_run
+    else
+      @in_perm || @in_run
+    end
   end
 
   def quote_keyval(key, val)
@@ -32,10 +45,10 @@ Puppet::Type.type(:firewalld_port).provide(
   end
 
   def create
-    execute_firewall_cmd(['--add-port', build_port_rule])
+    execute_firewall_cmd(['--add-port', build_port_rule]) unless @in_perm
   end
 
   def destroy
-    execute_firewall_cmd(['--remove-port', build_port_rule])
+    execute_firewall_cmd(['--remove-port', build_port_rule]) if @in_perm
   end
 end

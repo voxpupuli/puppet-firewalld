ce81bd72c36334ead3cb3cf8b8f6e2e1f10e4553
iqouiqa <ismail.qouiqa@capgemini.com>
2020-04-24 11:15:31 +0200

    revert service resource because it uses the transient reload

--- lib/puppet/provider/firewalld_service/firewall_cmd.rb
+++ lib/puppet/provider/firewalld_service/firewall_cmd.rb
@@ -7,25 +7,13 @@ Puppet::Type.type(:firewalld_service).provide(
 ) do
   desc 'Interact with firewall-cmd'
 
-  def initialize(value = {})
-    super(value)
-    @exists_in_perm = false
-    @exists_in_run = false
-  end
-
   def exists?
-    @exists_in_perm = execute_firewall_cmd(['--list-services']).split(' ').include?(@resource[:service])
-    @exists_in_run = execute_firewall_cmd(['--list-services'], nil, false).split(' ').include?(@resource[:service])
-    if @resource[:ensure] == :present
-      @exists_in_perm && @exists_in_run
-    else
-      @exists_in_perm || @exists_in_run
-    end
+    execute_firewall_cmd(['--list-services']).split(' ').include?(@resource[:service])
   end
 
   def create
     debug("Adding new service to firewalld: #{@resource[:service]}")
-    execute_firewall_cmd(['--add-service', @resource[:service]]) unless @exists_in_perm
+    execute_firewall_cmd(['--add-service', @resource[:service]])
     reload_firewall
   end
 
@@ -38,7 +26,7 @@ Puppet::Type.type(:firewalld_service).provide(
              '--remove-service-from-zone'
            end
 
-    execute_firewall_cmd([flag, @resource[:service]]) if @exists_in_perm
+    execute_firewall_cmd([flag, @resource[:service]])
     reload_firewall
   end
 end
